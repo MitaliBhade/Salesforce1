a3afbd40632d87b635f499b99c62e9087ee29db0
Mitali Bhade <63578995+undefined@users.noreply.github.com>
2022-04-22 11:11:31 +0000

    Push from ButtonBranch

--- src/app.js
+++ src/app.js
@@ -3,9 +3,19 @@ import { LightningElement, api } from "lwc";
 export default class App extends LightningElement {
 
   clickedButtonLabel;
+   buttonStatefulState = false;
+    buttonIconStatefulState = false;
 
     handleClick(event) {
         this.clickedButtonLabel = event.target.label;
+    }
+
+    handleButtonStatefulClick() {
+        this.buttonStatefulState = !this.buttonStatefulState;
+    }
+
+    handleButtonIconStatefulClick() {
+        this.buttonIconStatefulState = !this.buttonIconStatefulState;
     }
 
 }
