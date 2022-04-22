267dc72973fa9fe857b2f3664312ed2bb156261f
Mitali Bhade <63578995+undefined@users.noreply.github.com>
2022-04-22 07:20:57 +0000

    Buttons1

--- src/app.js
+++ src/app.js
@@ -1,29 +1,11 @@
 import { LightningElement, api } from "lwc";
 
 export default class App extends LightningElement {
-@api names;
-numberFieldValue;
-     bike = {
-        name: 'Electra X4',
-        picture: 'https://s3-us-west-1.amazonaws.com/sfdc-demo/ebikes/electrax4.jpg'
-    };
-    @api message = 'This is my bike site';
-    name = 'Electra X4';
-   description = 'A sweet bike built for comfort.';
-   category = 'Mountain';
-   material = 'Steel';
-   price = '$2,700';
-   pictureUrl = 'https://s3-us-west-1.amazonaws.com/sfdc-demo/ebikes/electrax4.jpg';
 
-   ready = false;
-   connectedCallback() {
-       setTimeout(() => {
-           this.ready = true;
-       }, 3000);
-   }
+  clickedButtonLabel;
 
-   handleNumberChange(event){
-   this.numberFieldValue = event.target.value;
-}
+    handleClick(event) {
+        this.clickedButtonLabel = event.target.label;
+    }
 
 }
